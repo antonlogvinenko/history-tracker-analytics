9d7c901b207597c2dcb267ec8e222823111c00b1
Anton Logvinenko <logvinenko@farpost.com>
2012-06-14 12:40:23 +1100

    Adding data fetch

--- src/history_tracker_analytics/collect.clj
+++ src/history_tracker_analytics/collect.clj
@@ -1,15 +1,16 @@
 (ns history-tracker-analytics.collect
     (:require [clojure.contrib.sql :as sql]
-              [clj-time.local :as time]))
+              [clj-time.local :as time])
+    (:use [clojureql.core :only (table select where)]))
 
 ;;Reading MySQL settings
 (def mysql-settings {:classname "com.mysql.jdbc.Driver" :subprotocol "mysql"})
 
-(defn add-row [parameters row]
+(defn- add-row [parameters row]
   (let [pair (.split row "=")]
     (assoc parameters (keyword (first pair)) (second pair))))
 
-(defn init-db [file]
+(defn- init-db [file]
   (reduce add-row mysql-settings (.split file "\n")))
 
 ;;Counting size
@@ -17,12 +18,12 @@
 (def distribution-file-name "distribution")
 (def print-freq 100000)
 
-(defn count-entry-size [{context :context state :state}]
+(defn- count-entry-size [{context :context state :state}]
   (+ (* 2 (count context))
      (* 2 (count state))
      known-entry-size-bytes))
 
-(defn conjoin [coll entry]
+(defn- conjoin [coll entry]
   (let [id (entry :id)
         key (.hashCode (select-keys entry [:type :user_space_id]))
         size (count-entry-size entry)
@@ -36,7 +37,7 @@
 (def sql-query "SELECT id, type, user_space_id, state, context from history")
 
 ;;buggy mysql jdbc driver here, rewrote standard clojure wrapper library
-(defn iterate-history-entries-with [db]
+(defn- iterate-history-entries-with [db]
   (do (Class/forName (db :classname))
       (let [conn (java.sql.DriverManager/getConnection
                                  (str "jdbc:" (db :subprotocol) ":" (db :subname))
@@ -64,3 +65,43 @@
         (println "MySQL connection parameters: " db)
         (spit statistics-file-name (vals big-data))
         (println end-time))))
+
+
+
+(def remote-mysql-config "remote.ini")
+(def local-mysql-config "local.ini")
+
+(def max-sql "select max(id) as extremeid from history")
+(def min-sql "select min(id) as extremeid from history")  
+(defn- get-id-extreme [sql]
+  (sql/with-query-results rs [sql] (:extremeid (nth rs 0))))
+(defn- get-object-null [next-id]
+  (sql/with-query-results rs
+    ["select user_space_id, type from history where id = ?" (next-id)] rs))
+(defn- get-object [next-id]
+  (loop [next-id next-id]
+    (let [object (get-object-null next-id)]
+      (if (nil? object) (recur next-id)
+          (let [object (nth object 0)]
+            {:user-space-id (object :user_space_id)
+             :type (object :type)})))))
+
+;;load entries
+;;save entries
+;;routine
+
+(defn- get-random-objects [amount]
+  (let [min-id (get-id-extreme min-sql)
+        max-id (get-id-extreme max-sql)
+        next-id #(+ (rand-int (+ max-id (- min-id) 1)) min-id)]
+    (loop [result #{}]
+      (if (= (count result) amount) result
+        (recur (conj result (get-object next-id)))))))
+        
+        
+(defn load-sample [amount]
+  (let [local-db (init-db (slurp local-mysql-config))
+        remote-db (init-db (slurp remote-mysql-config))]
+    (sql/with-connection remote-db
+      (time (get-random-objects amount)))))
+
